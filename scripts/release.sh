36d9485d8cf40cbe2f4f6a1b61bbfd6b7c4bb211
Salim Afiune Maya <afiune@lacework.net>
2020-10-23 17:43:10 +0200

    ci: fix scripts
    Signed-off-by: Salim Afiune Maya <afiune@lacework.net>

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,7 +9,7 @@
 set -eou pipefail
 
 readonly org_name=lacework
-readonly project_name=terraform-gcp-audit-log
+readonly project_name=terraform-azure-activity-log
 readonly git_user="Salim Afiune Maya"
 readonly git_email="afiune@lacework.net"
 VERSION=$(cat VERSION)
