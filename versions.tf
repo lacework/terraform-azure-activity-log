e5d04efab516ba969dac2bb818239be514dd2169
Alan Nix <alan.nix@lacework.net>
2021-07-13 09:48:51 -0400

    fix: corrected azure provider version constraints

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.12.31"
 
   required_providers {
-    azuread = ">= 0.11"
-    azurerm = "~> 2.26.0"
+    azuread = "~> 1.2"
+    azurerm = "~> 2.26"
     random  = ">= 2.1"
     lacework = {
       source  = "lacework/lacework"
