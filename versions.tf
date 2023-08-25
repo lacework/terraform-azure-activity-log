9941880d0ecf3dd7e98797d1173cb1bdc2a42079
Laurent Raufaste <analogue@glop.org>
2023-08-25 15:40:20 +0200

    fix: Replace deprecated `log` blocks with `enabled_log`
    The log block in azurerm_monitor_diagnostic_setting is deprecated, but starting from 3.71.0 of azurerm, it's now required to have an enabled_log block.
    
    Signed-off-by: Laurent Raufaste <analogue@glop.org>

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.14"
 
   required_providers {
-    azurerm = "~> 3.0"
+    azurerm = "~> 3.71"
     random  = ">= 2.1"
     lacework = {
       source  = "lacework/lacework"
