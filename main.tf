3535961b8b2ffb8d3252e00f0efafe76ba2a97ed
Darren Murray <darren.murray@lacework.net>
2024-02-12 17:24:50 +0000

    fix: lacework-global-534
    Signed-off-by: Darren Murray <darren.murray@lacework.net>

--- main.tf
+++ main.tf
@@ -236,6 +236,7 @@ resource "azurerm_subnet" "lacework" {
   resource_group_name  = azurerm_resource_group.lacework[0].name
   virtual_network_name = azurerm_virtual_network.lacework.name
   address_prefixes     = ["10.0.1.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
 
   enforce_private_link_endpoint_network_policies = true
 }
